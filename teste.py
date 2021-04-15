17bd99a45d49229518d9d1bc4e0acc377ccbafb9
cristianpaes <s4camargo@gmail.com>
2021-04-15 01:46:02 -0300

    Teste Atoz

---
+++ teste.py
@@ -0,0 +1,13 @@
+
+for cont in range(1,101):
+
+    if ((cont % 3)==0 and (cont % 7)==0):
+        cont = "JeffBezos"
+    elif (cont%3)==0:
+        cont = "Jeff"
+    elif(cont%7)==0:
+        cont = "Bezos"
+
+    print(cont)
+
+
